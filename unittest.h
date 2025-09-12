853581b03071b460763c2fa322f9763daf1374d9
Zephyr Lykos <git@mochaa.ws>
2025-09-13 00:12:49 +0800

    tests: fix memory leak in tests

--- unittest.h
+++ unittest.h
@@ -23,11 +23,11 @@ struct PYSTRINGTest
     PYSTRINGTestFunc function;
 };
 
-typedef std::vector<PYSTRINGTest*> UnitTests;
+typedef std::vector<PYSTRINGTest> UnitTests;
 
 UnitTests& GetUnitTests();
 
-struct AddTest { AddTest(PYSTRINGTest* test); };
+struct AddTest { AddTest(PYSTRINGTest&& test); };
 
 /// PYSTRING_CHECK_* macros checks if the conditions is met, and if not,
 /// prints an error message indicating the module and line where the
@@ -102,22 +102,22 @@ struct AddTest { AddTest(PYSTRINGTest* test); };
 
 #define PYSTRING_ADD_TEST(group, name)                                      \
     static void pystringtest_##group##_##name();                            \
-    AddTest pystringaddtest_##group##_##name(new PYSTRINGTest(#group, #name, pystringtest_##group##_##name)); \
+    AddTest pystringaddtest_##group##_##name(PYSTRINGTest(#group, #name, pystringtest_##group##_##name)); \
     static void pystringtest_##group##_##name()
 
 #define PYSTRING_TEST_SETUP() \
     int unit_test_failures = 0
 
 #define PYSTRING_TEST_APP(app)                                              \
-    std::vector<PYSTRINGTest*>& GetUnitTests() {                            \
-        static std::vector<PYSTRINGTest*> pystring_unit_tests;                  \
+    std::vector<PYSTRINGTest>& GetUnitTests() {                            \
+        static std::vector<PYSTRINGTest> pystring_unit_tests;                  \
         return pystring_unit_tests; }                                       \
-    AddTest::AddTest(PYSTRINGTest* test){GetUnitTests().push_back(test);};  \
+    AddTest::AddTest(PYSTRINGTest&& test){GetUnitTests().emplace_back(test);};  \
     PYSTRING_TEST_SETUP(); \
     int main(int, char **) { std::cerr << "\n" << #app <<"\n\n";        \
         for(size_t i = 0; i < GetUnitTests().size(); ++i) {             \
-            int _tmp = unit_test_failures; GetUnitTests()[i]->function(); \
-            std::cerr << "Test [" << GetUnitTests()[i]->group << "] [" << GetUnitTests()[i]->name << "] - "; \
+            int _tmp = unit_test_failures; GetUnitTests()[i].function(); \
+            std::cerr << "Test [" << GetUnitTests()[i].group << "] [" << GetUnitTests()[i].name << "] - "; \
             std::cerr << (_tmp == unit_test_failures ? "PASSED" : "FAILED") << "\n"; } \
         std::cerr << "\n" << unit_test_failures << " tests failed\n\n";   \
         return unit_test_failures; }
